4b751643d4d77f6cf73e6fd224cd47291227b59a
Mr-Sakil <142120431+Mr-Sakil@users.noreply.github.com>
2024-11-01 10:37:41 +0530

    Add files via upload

--- plugins/a_welcome.py
+++ plugins/a_welcome.py
@@ -170,7 +170,7 @@ async def greet_new_members(_, member: ChatMemberUpdated):
     except Exception as e:
         return
 
-__MODULE__ = "Wᴇᴄᴏᴍᴇ"
+__MODULE__ = "ᴡᴇʟᴄᴏᴍᴇ"
 __HELP__ = """
 ## Aᴜᴛᴏ-Wᴇᴄᴏᴍᴇ Mᴏᴅᴜᴇ Cᴏᴍᴍᴀɴᴅs
 
@@ -179,9 +179,8 @@ __HELP__ = """
 Eɴᴀʙᴇs ᴏʀ ᴅɪsᴀʙᴇs ᴛʜᴇ ᴀᴜᴛᴏ-ᴡᴇᴄᴏᴍᴇ ғᴇᴀᴛᴜʀᴇ ɪɴ ᴀ ɢʀᴏᴜᴘ ᴄʜᴀᴛ.
 
 **Usᴀɢᴇ:**
-/welcome [ᴏɴ|ᴏғғ] (ғᴏʀ ʙᴏᴛ)
-
 /awelcome [ᴏɴ|ᴏғғ] (ғᴏʀ ᴀssɪsᴛᴀɴᴄᴇ)
+/welcome [ᴏɴ|ᴏғғ] (ғᴏʀ ʙᴏᴛ)
 
 **Dᴇᴛᴀɪs:**
 - ᴏɴ: Eɴᴀʙᴇs ᴀᴜᴛᴏ-ᴡᴇᴄᴏᴍᴇ ɴᴏᴛɪғɪᴄᴀᴛɪᴏɴs.
